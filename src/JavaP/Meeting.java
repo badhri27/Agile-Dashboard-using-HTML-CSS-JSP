ec90fe9bed93bc5229380574918f0742d9d49207
badhri27 <42508198+badhri27@users.noreply.github.com>
2020-08-30 20:14:05 +0530

    Add files via upload

---
+++ src/JavaP/Meeting.java
@@ -0,0 +1,44 @@
+package JavaP;
+
+public class Meeting {
+	private int meetingid;
+	private String meetingtitle,meetingdate,meetingnote1,meetingnote2,meetingnote3;
+	public int getMeetingid() {
+		return meetingid;
+	}
+	public void setMeetingid(int meetingid) {
+		this.meetingid = meetingid;
+	}
+	public String getMeetingtitle() {
+		return meetingtitle;
+	}
+	public void setMeetingtitle(String meetingtitle) {
+		this.meetingtitle = meetingtitle;
+	}
+	public String getMeetingdate() {
+		return meetingdate;
+	}
+	public void setMeetingdate(String meetingdate) {
+		this.meetingdate = meetingdate;
+	}
+	public String getMeetingnote1() {
+		return meetingnote1;
+	}
+	public void setMeetingnote1(String meetingnote1) {
+		this.meetingnote1 = meetingnote1;
+	}
+	public String getMeetingnote2() {
+		return meetingnote2;
+	}
+	public void setMeetingnote2(String meetingnote2) {
+		this.meetingnote2 = meetingnote2;
+	}
+	public String getMeetingnote3() {
+		return meetingnote3;
+	}
+	public void setMeetingnote3(String meetingnote3) {
+		this.meetingnote3 = meetingnote3;
+	}
+	
+	
+}
